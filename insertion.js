70c5b4cf4871ebea97e17548abf56ad551dbef43
sam <wax654@gmail.com>
2021-08-18 18:17:18 -0700

    Finished INsertion

--- insertion.js
+++ insertion.js
@@ -1,3 +1,18 @@
-function insertionSort() {}
+function insertionSort(arr) {
+    if(arr.length <= 1) return arr;
+    let i = 1;
+    let j;
+    while(i < arr.length) {
+        j = i;
+        while (j > 0 && arr[j] < arr[j - 1]){
+            let temp = arr[j];
+            arr[j] = arr[j - 1];
+            arr[j - 1] = temp;
+            j--;
+        }
+        i++;
+    }
+    return arr;
+}
 
 module.exports = insertionSort;
